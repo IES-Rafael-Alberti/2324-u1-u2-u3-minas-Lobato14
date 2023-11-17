6f3446f9c33ca5a78405318e694ee6d95ef6b347
rubencelislobato@gmail.com <rubencelislobato@gmail.com>
2023-11-17 09:22:53 +0100

    Creacion del menú de buscaminas y creacion del archivo test_minas.py

--- src/minas.py
+++ src/minas.py
@@ -9,15 +9,54 @@ Este ejercicio es una excelente manera de evaluar y mejorar las habilidades de p
 
 import random
 
+def mostrarTablero():
+    """
+        Función que muestra el tablero    
+    """
+    filas = 8
+    columnas = 8
+    matriz = [['.' for _ in range(columnas)] for _ in range(filas)]
+
+    for fila in range(filas + 1):
+        for columna in range(columnas + 1):
+            if fila == 0 and columna == 0:
+                print(' ', end=' ')
+            elif fila == 0:
+                print(columna, end=' ')
+            elif columna == 0:
+                print(fila, end=' ')
+            else:
+                print(matriz[fila - 1][columna - 1], end=' ')
+        print()
+
+def mostrarMenu():
+    """
+        Función que muestra el menú del juego    
+    """
+    print("\n1. Revelar celda\n"+
+    "2. Marcar celda\n"
+    "3. Salir\n")
+
 def jugar():
     """
     Esta función ejecuta el juego.
 
     """
 
-
 if __name__ == "__main__":
     """
     Esta sección del código se ejecuta solo si ejecutamos este archivo directamente.
     """
+    opcion = ""
+    while opcion != "3":
+        mostrarTablero()
+        mostrarMenu()
+        opcion = input("Elija su elección: ")
+        if opcion == "1":
+            coordenadas = input("Ingrese las coordenadas (fila, columna): ")
+            
+        if opcion == "2":
+            """
+            
+            """
     jugar()
